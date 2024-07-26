f6d18ea3abfc5a9c425b486855a210ab2d8676c7
Giuly ¯\_(ツ)_/¯ <giulyferto@gmail.com>
2024-07-25 22:31:40 -0300

    landing page

---
+++ components/landing.tsx
@@ -0,0 +1,48 @@
+import { Box, Button, Grid } from "@mui/material";
+import styles from "./landing.module.css";
+
+const LandingPage = () => {
+  return (
+    <Box className={styles.landingContainer}>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          "&.MuiGrid-root": {
+            marginTop: "0px",
+          },
+        }}
+      >
+        <Grid item xs={4}>
+          <Box className={styles.textContainer}>
+            <Box className={styles.title}>
+              Explore a World of Style and Comfort
+            </Box>
+            <Box className={styles.description}>
+              Explore our handpicked selection of the latest men&apos;s fashion
+              trends. From classic suits to casual essentials, we have you
+              covered.
+            </Box>
+            <Button className={styles.shopNow} variant="contained">
+              SHOP NOW
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={8}>
+          <Box
+            className={styles.imageContainer}
+            sx={{
+              backgroundImage: "url('../landing-photo.jpg')",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "right",
+              height: 500,
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default LandingPage;
